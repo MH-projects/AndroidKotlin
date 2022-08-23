8146d191c3b10fe223228bc907e8189d1c599f4c
Antonio <antonio_br@tesch.edu.mx>
2022-08-23 12:12:58 -0500

    Feature - ActAntonioMain

---
+++ app/src/main/java/com/mh/basickotlin/ui/Antonio/ActAntonioMain.kt
@@ -0,0 +1,12 @@
+package com.mh.basickotlin.ui.Antonio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mh.basickotlin.R
+
+class ActAntonioMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_antonio_main)
+    }
+}
--- app/src/main/java/com/mh/basickotlin/ui/manuel/activity/ActManuelMain.kt
+++ app/src/main/java/com/mh/basickotlin/ui/manuel/activity/ActManuelMain.kt
@@ -1,22 +1,30 @@
 package com.mh.basickotlin.ui.manuel.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.mh.basickotlin.R
+import com.mh.basickotlin.ui.Antonio.ActAntonioMain
 
 class ActManuelMain : AppCompatActivity() {
 
     private lateinit var btnManuel: Button
+    private lateinit var btnAntonio:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_manuel_main)
 
         btnManuel = findViewById(R.id.btnManuel)
-
         btnManuel.setOnClickListener {
             println("Click Manuel")
+        }
+        btnAntonio = findViewById(R.id.btnAntonio)
+        btnAntonio.setOnClickListener {
+            println("Click Antonio")
+            val intent1 = Intent(this,ActAntonioMain::class.java)
+            startActivity(intent1)
         }
     }
 }
