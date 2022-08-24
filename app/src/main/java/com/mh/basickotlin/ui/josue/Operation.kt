cd4039231c83f7548c473780561319cb440d6d13
Manuel Hidalgo <manuelhidalgo@blue.gen.in>
2022-08-24 17:24:18 -0500

    Feature - "Calculator Changes Josue With Test"

---
+++ app/src/main/java/com/mh/basickotlin/ui/josue/Operation.kt
@@ -0,0 +1,21 @@
+package com.mh.basickotlin.ui.josue
+
+class Operation {
+
+    fun convertStringToInt(number: String): Int {
+        return if (number.isEmpty()) 0 else number.toInt()
+    }
+
+    fun suma(numUno: Int, numDos: Int): Int {
+        return numUno.plus(numDos)
+    }
+    fun resta(numUno: Int, numDos: Int): Int {
+        return numUno.minus(numDos)
+    }
+    fun multi(numUno: Int, numDos: Int): Int {
+        return numUno.times(numDos)
+    }
+    fun div(numUno: Int, numDos: Int): Int {
+        return numUno.div(numDos)
+    }
+}
