899bf2d583795581f5d79090c77fb6785e6e58bc
Antonio <antonio_br@tesch.edu.mx>
2022-08-25 17:42:10 -0500

    Feature- frgAntonioContent && FrgAntonioCalculator implement

--- app/src/main/java/com/mh/basickotlin/ui/Antonio/fragment/FrgAntonioContent.kt
+++ app/src/main/java/com/mh/basickotlin/ui/Antonio/fragment/FrgAntonioContent.kt
@@ -1,32 +1,29 @@
-package com.mh.basickotlin.ui.manuel.fragment
+package com.mh.basickotlin.ui.Antonio.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mh.basickotlin.R
 
-class FrgDevs : Fragment() {
+class FrgAntonioContent : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frg_devs, container, false)
+        return inflater.inflate(R.layout.frg_antonio_content, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.btnManuel).setOnClickListener {
-            findNavController().navigate(R.id.toFrgManuelCalculator)
-        }
-        view.findViewById<Button>(R.id.btnAntonio).setOnClickListener {
-            findNavController().navigate(R.id.toFrgAntonioContent)
+        view.findViewById<Button>(R.id.btnFrgAntonioCalculator).setOnClickListener {
+            findNavController().navigate(R.id.actionToFrgAntonioCalculadora)
         }
     }
 }
