da179f2427d937fb20ed02d3e51ceb8b51a223a3
PabloAmaya10 <amayagordillopabloalexis@gmail.com>
2022-08-25 17:43:15 -0500

    Features - FrgPabloContent and FrgPabloCalculator implement

--- app/src/main/java/com/mh/basickotlin/ui/manuel/fragment/FrgDevs.kt
+++ app/src/main/java/com/mh/basickotlin/ui/manuel/fragment/FrgDevs.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mh.basickotlin.R
@@ -30,6 +31,9 @@ class FrgDevs : Fragment() {
         }
         view.findViewById<Button>(R.id.btnAngel).setOnClickListener {
             findNavController().navigate(R.id.frgAngelMain)
+        }
+        view.findViewById<Button>(R.id.btnPablo).setOnClickListener {
+            findNavController().navigate(R.id.toFrgPabloContent)
         }
     }
 }
