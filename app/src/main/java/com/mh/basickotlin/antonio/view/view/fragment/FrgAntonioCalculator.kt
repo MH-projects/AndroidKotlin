52c4ed97487324531b7b83b7e728909c76b85a46
Antonio <antonio_br@tesch.edu.mx>
2022-08-30 17:35:33 -0500

    [JAP-19] [Feature] - CalculatorMVVM

--- app/src/main/java/com/mh/basickotlin/antonio/view/view/fragment/FrgAntonioCalculator.kt
+++ app/src/main/java/com/mh/basickotlin/antonio/view/view/fragment/FrgAntonioCalculator.kt
@@ -4,24 +4,31 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import com.mh.basickotlin.databinding.FrgAntonioCalculatorBinding
 import com.mh.basickotlin.antonio.clasesAntonio.Calculadora
 import com.mh.basickotlin.antonio.model.Digitos
 import com.mh.basickotlin.antonio.model.Operadores
 import com.mh.basickotlin.antonio.presenter.AntonioCalculadorContract
 import com.mh.basickotlin.antonio.presenter.AntonioCalculadoraPresenter
+import com.mh.basickotlin.antonio.viewmodel.CalculadoraViewModel
 
-class FrgAntonioCalculator : Fragment(),AntonioCalculadorContract.view {
+//class FrgAntonioCalculator : Fragment(),AntonioCalculadorContract.view {
+class FrgAntonioCalculator : Fragment(){
 
     private lateinit var binding: FrgAntonioCalculatorBinding
 
     private lateinit var presenter: AntonioCalculadorContract.presenter
 
-    
+    val viewModel:CalculadoraViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -34,7 +41,7 @@ class FrgAntonioCalculator : Fragment(),AntonioCalculadorContract.view {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        presenter = AntonioCalculadoraPresenter(this)
+        //presenter = AntonioCalculadoraPresenter(this)
 
        /* binding.etInsertNum.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
@@ -63,38 +70,46 @@ class FrgAntonioCalculator : Fragment(),AntonioCalculadorContract.view {
             } else {
                 binding.btnIgual.visibility = View.VISIBLE
             }
-            presenter.autoResult(ischecked)
+            viewModel.autoResult(ischecked)
         }
-        binding.btnUno.setOnClickListener { presenter.checkValidDigit(Digitos.uno) }
-        binding.btnDos.setOnClickListener { presenter.checkValidDigit(Digitos.dos) }
-        binding.btnTres.setOnClickListener { presenter.checkValidDigit(Digitos.tres) }
-        binding.btnCuatro.setOnClickListener { presenter.checkValidDigit(Digitos.cuatro)}
-        binding.btnCinco.setOnClickListener { presenter.checkValidDigit(Digitos.cinco) }
-        binding.btnSeis.setOnClickListener { presenter.checkValidDigit(Digitos.seis) }
-        binding.btnSiete.setOnClickListener { presenter.checkValidDigit(Digitos.siete) }
-        binding.btnOcho.setOnClickListener { presenter.checkValidDigit(Digitos.ocho) }
-        binding.btnNueve.setOnClickListener { presenter.checkValidDigit(Digitos.nueve) }
-        binding.btnNumCero.setOnClickListener {presenter.checkValidDigit(Digitos.cero) }
 
-        binding.btnSuma.setOnClickListener { presenter.checkOperator(Operadores.suma) }
-        binding.btnResta.setOnClickListener { presenter.checkOperator(Operadores.resta) }
-        binding.btnMult.setOnClickListener { presenter.checkOperator(Operadores.mult) }
-        binding.btnDiv.setOnClickListener { presenter.checkOperator(Operadores.div) }
+        viewModel.textResultado.observe(viewLifecycleOwner){resultado->
+            binding.tvResult.text=resultado
+        }
+        viewModel.textOperacion.observe(viewLifecycleOwner){operacion->
+            binding.etInsertNum.setText(operacion)
+        }
 
-        binding.btnIgual.setOnClickListener { presenter.result() }
-        binding.btnclear.setOnClickListener { presenter.clear() }
-        binding.btnBorrar.setOnClickListener {presenter.delete()}
+        binding.btnUno.setOnClickListener { viewModel.checkValidDigit(Digitos.uno) }
+        binding.btnDos.setOnClickListener { viewModel.checkValidDigit(Digitos.dos) }
+        binding.btnTres.setOnClickListener { viewModel.checkValidDigit(Digitos.tres) }
+        binding.btnCuatro.setOnClickListener { viewModel.checkValidDigit(Digitos.cuatro)}
+        binding.btnCinco.setOnClickListener { viewModel.checkValidDigit(Digitos.cinco) }
+        binding.btnSeis.setOnClickListener { viewModel.checkValidDigit(Digitos.seis) }
+        binding.btnSiete.setOnClickListener { viewModel.checkValidDigit(Digitos.siete) }
+        binding.btnOcho.setOnClickListener { viewModel.checkValidDigit(Digitos.ocho) }
+        binding.btnNueve.setOnClickListener { viewModel.checkValidDigit(Digitos.nueve) }
+        binding.btnNumCero.setOnClickListener {viewModel.checkValidDigit(Digitos.cero) }
+
+        binding.btnSuma.setOnClickListener { viewModel.checkOperator(Operadores.suma) }
+        binding.btnResta.setOnClickListener { viewModel.checkOperator(Operadores.resta) }
+        binding.btnMult.setOnClickListener { viewModel.checkOperator(Operadores.mult) }
+        binding.btnDiv.setOnClickListener { viewModel.checkOperator(Operadores.div) }
+
+        binding.btnIgual.setOnClickListener { viewModel.result() }
+        binding.btnclear.setOnClickListener { viewModel.clear() }
+        binding.btnBorrar.setOnClickListener {viewModel.delete()}
 
 
     }
 
-    override fun setOP(op: String) {
+    /*override fun setOP(op: String) {
         binding.etInsertNum.text=op
     }
 
     override fun setResultados(result: String) {
        binding.tvResult.text= result
-    }
+    }*/
 
 
 }
